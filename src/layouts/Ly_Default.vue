04ff8fab59fbe42da587d0e0d68f2abea1a9f947
김영우 <kyw05171@gmail.com>
2021-07-16 18:53:39 +0900

    <김영우>: ``기본 레이아웃`` 컴포넌트를 작성 하였습니다.
    * ``메뉴``와 ``컨텐츠`` 영역으로 구성되어 있습니다.
    * 라우트에 ``기본 레이아웃``을 등록 하였습니다. (``:id`` 동적 라우팅)

---
+++ src/layouts/Ly_Default.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="Ly_Default">
+		<!-- 메뉴 영역 -->
+		<nav class="menuWrapper">
+			<Bl_Menu title="스마트 잭"></Bl_Menu>
+		</nav>
+
+		<!-- 컨텐츠 라이터 영역 -->
+		<main class="mainWrapper">
+			<div class="main__test">Main 테스트 입니다.</div>
+		</main>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Bl_Menu from "@/components/menu/Bl_Menu.vue";
+
+export default Vue.extend({
+	components: {
+		Bl_Menu,
+	},
+});
+</script>
+
+<style scoped lang="scss">
+#Ly_Default {
+	@include width-height(100%, 100%);
+	@include flex(flex-start, flex-start, nowrap);
+
+	background-color: rgba(#ff1493, 0.2);
+
+	$menu-width: 250px;
+
+	// 메뉴 영역
+	.menuWrapper {
+		@include width-height($menu-width, 100%);
+		min-width: $menu-width;
+
+		padding: 5px 15px 5px 10px;
+
+		background-color: $white-02;
+		box-shadow: -5px 0 5px rgba(#333, 0.1) inset;
+	}
+
+	// 메인 영역
+	.mainWrapper {
+		@include width-height(100%, 100%);
+
+		background-color: $white-03;
+	}
+}
+</style>
