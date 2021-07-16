a46d2a96034801ee2e6fc91b7766698891510253
김영우 <kyw05171@gmail.com>
2021-07-16 20:13:01 +0900

    <김영우>: 컨텐츠 페이지 컴포넌트를 작성 하였습니다.
    * ``Menu Store``의 ``targetMenu``에 따라 페이지를 출력 합니다.

---
+++ src/views/Page_DynamicContent.vue
@@ -0,0 +1,51 @@
+<template>
+	<article class="mainContent">
+		<section class="mainContent__section">
+			<h1 class="mainContent__title">
+				{{ contents }}
+			</h1>
+		</section>
+	</article>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { IMenu } from "@/insterface/IMenu";
+
+export default Vue.extend({
+	computed: {
+		contents(): string {
+			const targetMenu = this.$store.getters[
+				"/menu/getTargetMenu"
+			] as IMenu | null;
+
+			if (targetMenu) {
+				return targetMenu.title;
+			} else {
+				return "메뉴를 선택해 주세요";
+			}
+		},
+	},
+});
+</script>
+
+<style scoped lang="scss">
+.mainContent {
+	@include width-height(100%, 100%);
+
+	overflow-y: auto;
+
+	&__section {
+		//
+	}
+
+	&__title {
+		margin-top: 4rem;
+
+		color: $gray-03;
+		font-size: 2rem;
+		font-weight: 900;
+		text-align: center;
+	}
+}
+</style>
