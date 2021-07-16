3612baa348d9f1471973ae7ac2b3b04f6c4296a3
김영우 <kyw05171@gmail.com>
2021-07-16 18:35:42 +0900

    <김영우>: Root Store 를 작성 하였습니다.
    * ``NO_DATA``: 현재는 Root Store 에서 처리할 데이터는 없습니다.

---
+++ src/store/IRootStore.ts
@@ -0,0 +1,12 @@
+/**
+ * @author: 김영우
+ * @description:
+ *    * Root Store 에 대한 interface 입니다.
+ *    * 관리할 데이터는 없지만, 전역 Store 구조를 위해 작성 하였습니다.
+ */
+
+interface IRootStore {
+	NO_DATA: undefined;
+}
+
+export { IRootStore };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { IRootStore } from "@/store/IRootStore";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	modules: {},
+export default new Vuex.Store<IRootStore>({
+	state: {
+		NO_DATA: undefined,
+	},
 });
