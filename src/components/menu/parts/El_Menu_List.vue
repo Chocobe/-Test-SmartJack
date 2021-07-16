0e8e5958300fec101f24d12f51702a78984ee50b
김영우 <kyw05171@gmail.com>
2021-07-16 19:35:38 +0900

    <김영우>: 메뉴 리스트 컴포넌트를 작성 하였습니다.
    * ``goToPage()``: 클릭 시, 페이지 이동 메서드 입니다.

---
+++ src/components/menu/parts/El_Menu_List.vue
@@ -0,0 +1,84 @@
+<template>
+	<ul class="menuList">
+		<template v-for="menu in menuList">
+			<li
+				class="menuList__item"
+				:class="{ 'menuList__item--target': menu.id === targetMenuId }"
+				:key="`menuList-${menu.id}`"
+				@click="goToPage(menu)"
+			>
+				{{ menu.title }}
+			</li>
+		</template>
+	</ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { IMenu } from "@/insterface/IMenu";
+
+export default Vue.extend({
+	computed: {
+		menuList(): IMenu[] {
+			return this.$store.getters["/menu/getMenuList"];
+		},
+
+		targetMenuId(): string {
+			const targetMenu = this.$store.getters["/menu/getTargetMenu"] as IMenu;
+			return targetMenu.id;
+		},
+	},
+
+	methods: {
+		goToPage(menu: IMenu) {
+			const menuId = menu.id;
+			const routeId = this.$route.params.id;
+
+			if (menuId === routeId) {
+				return;
+			}
+
+			this.$store.commit("/menu/setTargetMenu", menu);
+			this.$router.push(`/${menuId}`);
+
+			this.$emit("click");
+		},
+	},
+});
+</script>
+
+<style scoped lang="scss">
+.menuList {
+	//
+
+	&__item {
+		padding: 5px 5px 5px 20px;
+
+		font-size: 0.83rem;
+		font-weight: 900;
+
+		border-radius: 5px;
+
+		position: relative;
+
+		cursor: default;
+
+		&::before {
+			@include width-height(5px, 5px);
+
+			content: "";
+
+			position: absolute;
+			top: 50%;
+			left: 7.5px;
+			transform: translateY(-50%);
+
+			background-color: $gray-01;
+		}
+
+		&--target {
+			background-color: $yellow-01;
+		}
+	}
+}
+</style>
